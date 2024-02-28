23959e9cd5fc1ea0402a115997ad98fd72aa1f86
Goran Ninkovic <g.ninkovic@angeltech.rs>
2024-02-28 14:47:26 +0100

    Add ESLint and Prettier configurations, update linting scripts, and add Postcss and Tailwind CSS support.

---
+++ .eslintrc.cjs
@@ -0,0 +1,26 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  parser: "vue-eslint-parser",
+  parserOptions: {
+    parser: "@typescript-eslint/parser",
+  },
+  extends: [
+    "plugin:vue/vue3-recommended",
+    "plugin:prettier/recommended",
+    "@vue/eslint-config-prettier/skip-formatting",
+  ],
+  plugins: [],
+  rules: {},
+  overrides: [
+    {
+      rules: {
+        "vue/multi-word-component-names": "off",
+      },
+      files: ["**/views/**/*.vue"],
+    },
+  ],
+}
