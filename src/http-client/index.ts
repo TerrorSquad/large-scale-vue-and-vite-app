aedb7ab71cd00296c79fcb0fa7ad79c44519de29
Goran Ninkovic <g.ninkovic@angeltech.rs>
2024-02-28 16:56:50 +0100

    Add http-client module with useHttpClient function

---
+++ src/http-client/index.ts
@@ -0,0 +1,24 @@
+import { HttpClientInterface } from './models/HttpClient.interface'
+
+import { HttpClientAxios } from './models/HttpClient.axios'
+import { HttpClientFetch } from './models/HttpClient.fetch'
+
+export * from './models'
+
+let _httpClient: HttpClientInterface | undefined = undefined
+
+export const useHttpClient = () => {
+  if (!_httpClient) {
+    // export instance of HttpClientInterface
+    const clientType = 'fetch'
+    // const clientType = config.httpClient.clientType
+    // later will drive from config
+    // if you'd like to use axios, set "clientType": "axios" within the config files httpClient section
+    if (clientType === 'fetch') {
+      _httpClient = new HttpClientFetch()
+    } else if (clientType === 'axios') {
+      _httpClient = new HttpClientAxios()
+    }
+  }
+  return _httpClient as HttpClientInterface
+}
