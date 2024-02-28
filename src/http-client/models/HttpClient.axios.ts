e18657fffaef9ae4a89969fa614ecbc2eec24cad
Goran Ninkovic <g.ninkovic@angeltech.rs>
2024-02-28 16:37:15 +0100

    Add HttpClientAxios implementation and related constants and interfaces

---
+++ src/http-client/models/HttpClient.axios.ts
@@ -0,0 +1,60 @@
+import axios, { AxiosRequestConfig, AxiosResponse } from 'axios'
+
+import { HttpRequestParamsInterface } from './HttpRequestParams.interface'
+import { HttpClientInterface, HttpClientConfigInterface } from './HttpClient.interface'
+import { HttpRequestType, HttpContentTypes } from './Constants'
+import { UrlUtils } from './UrlUtils'
+
+/**
+ * @name HttpClientAxios
+ * @description
+ * Wraps http client functionality to avoid directly using a third party npm package like {@link axios}
+ * and simplify replacement in the future if such npm package would stop being developed or other reasons
+ */
+export class HttpClientAxios implements HttpClientInterface {
+  request<R, P = void>(parameters: HttpRequestParamsInterface<P>): Promise<R> {
+    const { requestType, endpoint, requiresToken, payload, headers, mockDelay } = parameters
+
+    const fullUrl = UrlUtils.getFullUrlWithParams(endpoint, payload as any)
+    console.log('HttpClientAxios: request: fullUrl', fullUrl, payload)
+
+    // set axios options
+    const options: AxiosRequestConfig = {
+      headers: {},
+      maxRedirects: 0,
+    }
+    if (headers) {
+      options.headers = {
+        ...options.headers,
+        ...headers,
+      }
+    }
+
+    // set headers authorization
+    if (requiresToken && options.headers) {
+      options.withCredentials = true
+      // optional: you could set Bearer token here
+      // options.headers.Authorization = `Bearer ${ JwtHelpers.getJwtToken() }`
+    }
+    let result!: R
+    try {
+      switch (requestType) {
+        // TODO: implement other request types
+        default:
+          console.warn('HttpClientAxios: request: requestType not implemented', requestType)
+      }
+    } catch (e) {
+      console.error('HttpClientAxios: request: error', e)
+      throw Error('HttpClientAxios: request: error')
+    }
+
+    if ((mockDelay || 0) > 0) {
+      return new Promise<R>((resolve) => {
+        setTimeout(() => {
+          resolve(result)
+        }, mockDelay)
+      })
+    }
+    return Promise.resolve(result)
+  }
+}
---
+++ src/http-client/models/UrlUtils.ts
@@ -0,0 +1,18 @@
+export interface UrlUtilsInterface {
+  getFullUrlWithParams(baseUrl: string, params: { [key: string]: string | number }): string
+}
+
+export const UrlUtils: UrlUtilsInterface = {
+  getFullUrlWithParams(baseUrl: string, params: { [key: string]: string | number }): string {
+    const keys: string[] = Object.keys(params || {})
+    if ((baseUrl || '').indexOf('[') === -1 || keys.length === 0) {
+      return baseUrl
+    }
+    let fullUrl = baseUrl
+    keys.forEach((key) => {
+      const value = params[key]
+      fullUrl = fullUrl.replace(`[${key}]`, (params[key] || '').toString())
+    })
+    return fullUrl
+  },
+}
