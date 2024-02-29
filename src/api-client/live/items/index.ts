4fecfbb41d2cce52f328fafe2197582772489912
Goran Ninkovic <g.ninkovic@angeltech.rs>
2024-02-29 15:48:03 +0100

    Refactor import statements and update API endpoint

--- src/api-client/live/items/index.ts
+++ src/api-client/live/items/index.ts
@@ -1,16 +1,9 @@
-import {
-  ItemsApiClientOptions,
-  ItemsApiClientInterface,
-  ItemsApiClientModel,
-} from '../../models/items'
+import { config } from '@/config'
+import { ItemsApiClientInterface, ItemsApiClientModel } from '../../models/items'
 
-const options: ItemsApiClientOptions = {
-  endpoints: {
-    // this should be pointing to the live API end-point
-    fetchItems: '/path/to/your/real/api/end-point',
-  },
-}
 // instantiate the ItemsApiClient pointing at the url that returns live data
-const itemsApiClient: ItemsApiClientInterface = new ItemsApiClientModel(options)
+const itemsApiClient: ItemsApiClientInterface = new ItemsApiClientModel(
+  config.items.apiClientOptions,
+)
 // export our instance
 export { itemsApiClient }
