1150d2507a623138ce3969e7b4a6013b2223d231
Goran Ninkovic <g.ninkovic@angeltech.rs>
2024-02-29 15:45:04 +0100

    Update environment and client configuration

--- src/api-client/index.ts
+++ src/api-client/index.ts
@@ -1,14 +1,8 @@
-import { ApiClientInterface } from './models'
 import { apiMockClient } from './mock'
 import { apiLiveClient } from './live'
 
-let env: string = 'mock'
-
-console.log('import.meta.env', JSON.stringify(import.meta.env))
-if (import.meta.env?.VITE_API_CLIENT) {
-  env = import.meta.env.VITE_API_CLIENT.trim()
-}
+import { config } from '@/config'
 
 // return either the live or the mock client
-const apiClient: ApiClientInterface = env === 'mock' ? apiMockClient : apiLiveClient
+const apiClient = config.apiClient.type === 'live' ? apiLiveClient : apiMockClient
 export { apiClient }
--- src/http-client/index.ts
+++ src/http-client/index.ts
@@ -1,5 +1,7 @@
 import { HttpClientInterface } from './models/HttpClient.interface'
 
+import { config } from '@/config'
+
 import { HttpClientAxios } from './models/HttpClient.axios'
 import { HttpClientFetch } from './models/HttpClient.fetch'
 
@@ -10,12 +12,8 @@ let _httpClient: HttpClientInterface | undefined = undefined
 export const useHttpClient = () => {
   if (!_httpClient) {
     // export instance of HttpClientInterface
-    const clientType = 'fetch'
     // TODO: Implement another library like axios
-    // TODO: Implement a way to switch between libraries in the config file or environment variables
-    // const clientType = config.httpClient.clientType
-    // later will drive from config
-    // if you'd like to use axios, set "clientType": "axios" within the config files httpClient section
+    const clientType = config.httpClient.clientType
     if (clientType === 'fetch') {
       _httpClient = new HttpClientFetch()
     } else if (clientType === 'axios') {
